92c676541a8c6b85277bbac5f22cb30ed604d366
Brian Woodward <brian.woodward@gmail.com>
2015-07-27 12:50:09 -0400

    complete refactor, remove all existing code

--- cli.js
+++ cli.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
 
-var path = require('path');
-var argv = require('minimist')(process.argv.slice(2));
-var app = require('./');
-app.enable('verbose');
-
-require(path.join(process.cwd(), 'example.js'));
-app.run.apply(app, argv._);
+var composer = require('./');
+console.log('TODO: setup composer cli');
