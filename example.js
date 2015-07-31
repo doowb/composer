06ce64d9998bf097821a4db9b2fddac2f427ead7
Brian Woodward <brian.woodward@gmail.com>
2015-07-31 13:00:19 -0400

    updating example to use template and render some pages

--- example.js
+++ example.js
@@ -1,179 +1,91 @@
 'use strict';
 
-// require('time-require');
-var lazy = require('lazy-cache')(require);
-var bluebird = lazy('bluebird');
-var through = lazy('through2');
-var green = lazy('ansi-green');
-var yellow = lazy('ansi-yellow');
-var red = lazy('ansi-red');
+var matter = require('parser-front-matter');
+var extname = require('gulp-extname');
+var through = require('through2');
+var path = require('path');
 
 var composer = require('./');
+var Template = require('template');
+var template = new Template();
 
-// setup some listeners
-composer.on('task.starting', function (task) {
-  var start = new Date();
-  console.log(green()('starting [' + task.name + ']'), start.toTimeString());
-});
-composer.on('task.finished', function (task) {
-  var end = new Date();
-  console.log(yellow()('finished [' + task.name + ']'), end.toTimeString());
+template.engine('hbs', require('engine-handlebars'));
+template.option('renameKey', function (fp) {
+  return path.basename(fp, path.extname(fp));
 });
 
-composer.on('error', function (err, task) {
-  console.log(red()('[' + task.name + '] ERROR:'), err);
+template.onLoad(/\.hbs$/, function (file, next) {
+  matter.parse(file, next);
 });
 
-
-// register some tasks
-// var i = 0;
-composer.register('foo-sync', function (done) {
-  console.log('foo-sync');
-  done(null, 'foo-sync');
-});
-
-composer.register('foo-async', function (done) {
-  logAfter('foo-async', 3500, done.bind(null, null));
-});
-
-composer.register('foo-promise', function () {
-  var Promise = bluebird();
-  var promise = new Promise(function (resolve, reject) {
-    logAfter('foo-promise', 1000, resolve);
-  });
-  return promise;
-});
-
-composer.register('foo-stream', function () {
-  var stream = through().obj();
-  logAfter('foo-stream', 500, function (msg) {
-    stream.write(msg);
-    stream.end();
-  });
-  return stream;
-});
-
-composer.register('beep', [
-    function (done) { console.log(this.name); done(null, this.name); },
-    'foo-async',
-    function inline (done) { console.log(this.name); done(null, this.name); }
-  ], function (done) {
-    logAfter('beep', 2000, done.bind(null, null));
-  });
-
-composer.register('baz-with-deps', {flow: 'series'}, ['foo-sync', 'foo-async', 'foo-promise', 'foo-stream'], function (done) {
-  console.log('baz-with-deps\' dependencies finished');
-  logAfter('baz-with-deps', 3000, done.bind(null, null));
-});
-
-// composer.register('website', ['styles', 'scripts', 'templates'], function (done) {
-//   // async.series([
-//   //   composer.register('foo', [''])
-//   //   composer.run('styles');
-//   //   composer.run('scripts');
-//   //   composer.run('templates')
-//   // ]);
-// });
-
-
-// composers.register('foo', ['a', 'b', 'c'], function () {
-
-// })
-
-// composers.register('bar', ['a', 'b', 'c'], function () {
-
-// })
-
-// composer.register('watcher', function () {
-//   composer.watch(['**/*.js'], ['foo']);
-// });
-
-// composer.run('watcher');
-
-// composer.register('baz', ['foo', 'bar']);
-
-// var queue = [];
-
-// {
-//   'foo': ['a', 'b', 'c'],
-//   'bar': ['a', 'b', 'c'],
-//   'baz': ['foo', 'bar']
-// }
-
-
-// composer.register('blog', {site: 'This is my blog'}, require('website-boilerplate')());
-
-
-// composer.register('default', {flow: 'parallel'}, 'beep', 'baz-with-deps');
-composer.register('default', 'beep', 'baz-with-deps');
-
-// composer.run('beep', function () {
-//   console.log('done');
-// });
-
-// console.log(composer.tasks);
-
-// composer.run(['foo-sync', 'foo-async'], function () {
-//   console.log('done');
-//   // process.exit();
-// });
-
-composer.run('default', function (err, results) {
-  console.log('done');
-  console.log(JSON.stringify(results, null, 2));
-  // process.exit();
-});
-
-// composer.register('js', function (done) {
-//   console.log('javascript files changed');
-//   done();
-// });
-
-// composer.register('md', function (done) {
-//   console.log('markdown files changed');
-//   process.exit();
-//   done();
-// });
-
-// composer.watch('*.js', 'js');
-// composer.watch('*.md', 'md');
-// process.exit();
-// runSchedule(2);
-
-function runSchedule (max) {
-  max = typeof max === 'undefined' ? 2 : max;
-  var count = 0;
-  var schedule = composer.schedule('default');
-  // schedule.on('task.error', function (err, task) {
-  //   console.log('error', err, task.name);
-  //   console.log();
-  // });
-  // schedule.on('task.starting', function (task) {
-  //   console.log('task.starting', task.name);
-  // });
-  // schedule.on('task.finished', function (task) {
-  //   console.log('task.finished', task.name);
-  //   console.log();
-  // });
-  schedule.on('finished', function () {
-    console.log('finished', count++);
-    console.log(schedule.history);
-    console.log();
-    if (count < max) {
-      setTimeout(function () {
-        schedule.start();
-      }, 1000);
-    }
-  });
-
-  if (count < max) {
-    schedule.start();
-  }
+var paths = {
+  pages: ['examples/templates/pages/**/*.hbs'],
+  layouts: ['examples/templates/layouts/*.hbs'],
+  includes: ['examples/templates/includes/*.hbs']
 };
 
-function logAfter(msg, ms, done) {
-  setTimeout(function () {
-    console.log(msg);
-    done(msg);
-  }, 0);
+template.create('pages');
+template.create('layouts', {viewType: 'layout'});
+template.create('includes', {viewType: 'partial'});
+
+composer.register('layouts', function (done) {
+  template.layouts(paths.layouts);
+  done();
+});
+
+composer.register('includes', function (done) {
+  template.includes(paths.includes);
+  done();
+});
+
+composer.register('pages', function (done) {
+  template.pages(paths.pages);
+  done();
+});
+
+composer.register('site', function () {
+  return loadCollection('pages')
+    .pipe(render())
+    .pipe(extname())
+    .pipe(dest('examples/dist'));
+});
+
+composer.register('watch', function () {
+  composer.watch(paths.layouts, ['layouts', 'pages', 'site']);
+  composer.watch(paths.includes, ['includes', 'pages', 'site']);
+  composer.watch(paths.pages, ['pages', 'site']);
+});
+
+composer.register('default', ['layouts', 'includes', 'pages', 'site']);
+composer.register('dev', ['default', 'watch']);
+
+composer.run('dev', function (err, results) {
+  if (err) return console.error(err);
+  console.log('Finshed');
+});
+
+
+function loadCollection (collection) {
+  var stream = through.obj();
+  function write () {
+    template[collection].forOwn(function (view) {
+      stream.write(view);
+    });
+    stream.end();
+  }
+  process.nextTick(write);
+  return stream;
+}
+
+function render (locals) {
+  locals = locals || {};
+  return through.obj(function (file, enc, next) {
+    file.render(locals, next);
+  });
+}
+
+function dest (dir) {
+  return through.obj(function (file, enc, next) {
+    file.write(path.join(dir, path.basename(file.path)), next);
+  });
 }
