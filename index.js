8b1d5d381f307e691ad57c5bb0d24952235257a5
Brian Woodward <brian.woodward@gmail.com>
2015-07-31 12:13:26 -0400

    wrapping task functions to capture start, finish, and error events

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 'use strict';
 
-var lazy = require('lazy-cache')(require);
 var Emitter = require('component-emitter');
+var lazy = require('lazy-cache')(require);
+
 var isObject = lazy('isobject');
 var chokidar = lazy('chokidar');
 var bach = lazy('bach');
+
+var yellow = lazy('ansi-yellow');
+var green = lazy('ansi-green');
+var red = lazy('ansi-red');
 
 var Task = require('./lib/task');
 var noop = require('./lib/noop');
 var map = require('./lib/map-deps');
 var resolve = require('./lib/resolve');
+
+var colors = {
+  'starting': green,
+  'finished': yellow,
+  'error': red
+};
 
 function Composer (config) {
   Emitter.call(this);
@@ -40,10 +51,25 @@ Composer.prototype.register = function(name/*, options, dependencies and task */
     options: options,
     fn: fn
   });
+  task.on('starting', this.handleTask.bind(this, 'starting'));
+  task.on('finished', this.handleTask.bind(this, 'finished'));
+  task.on('error', this.handleError.bind(this, 'error'));
 
   this.tasks[name] = task;
   return this;
 };
+
+Composer.prototype.handleTask = function(event, task, run) {
+  var color = colors[event]();
+  var timeMethod = event === 'starting' ? 'start' : 'end';
+  console.log(color(event.toUpperCase() + ':'), task.name, run[timeMethod].toTimeString());
+};
+
+Composer.prototype.handleError = function (event, err, task, run) {
+  var color = colors[event]();
+  console.log(color(event.toUpperCase() + ':'), task.name, run.end.toTimeString());
+  console.log(color(event.toUpperCase() + ':'), err.stack);
+}
 
 Composer.prototype.run = function(/* list of tasks/functions to run */) {
   var args = [].concat.apply([], [].slice.call(arguments));
