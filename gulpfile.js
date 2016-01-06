d43ed923ef69a93db2098e3f9dbd61dca3198286
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-06 13:35:42 -0500

    use eslint

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,23 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var eslint = require('gulp-eslint');
-var formatter = require('eslint-friendly-formatter');
 
-var lint = ['index.js', 'lib/**/*.js'];
-
-gulp.task('coverage', function () {
-  return gulp.src(lint)
-    .pipe(istanbul())
+gulp.task('coverage', function() {
+  return gulp.src(['index.js', 'lib/*.js'])
+    .pipe(istanbul({includeUntested: true}))
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['coverage'], function () {
+gulp.task('mocha', ['coverage'], function() {
   return gulp.src('test/*.js')
-    .pipe(mocha({reporter: 'spec'}))
-    .pipe(istanbul.writeReports())
-    .pipe(istanbul.writeReports({
-      reporters: [ 'text' ],
-      reportOpts: {dir: 'coverage', file: 'summary.txt'}
-    }))
+    .pipe(mocha())
+    .pipe(istanbul.writeReports());
 });
 
-gulp.task('lint', function () {
-  return gulp.src(lint.concat('test/*.js'))
+gulp.task('eslint', function() {
+  return gulp.src(['*.js', 'lib/*.js', 'test/*.js'])
     .pipe(eslint())
-    .pipe(eslint.format(formatter));
+    .pipe(eslint.format());
 });
 
-gulp.task('default', ['lint', 'test']);
+gulp.task('default', ['mocha', 'eslint']);
