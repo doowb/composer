887dfaf37254e60294f22b89b38196aeb46b592d
jonschlinkert <jon.schlinkert@sellside.com>
2018-04-02 00:09:24 -0400

    clean up

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const Emitter = require('@sellside/emitter');
 const Composer = require('./lib/tasks');
-module.exports = Composer(require('@sellside/emitter'));
+
+module.exports = Composer(Emitter);
+module.exports.Task = require('./lib/task');
 module.exports.create = Composer;
