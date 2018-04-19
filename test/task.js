8d4c8a45e8fba00e606b063dcaf3d6fb46bed549
jonschlinkert <jon.schlinkert@sellside.com>
2018-04-19 11:02:01 -0400

    use Emitter

--- test/task.js
+++ test/task.js
@@ -3,7 +3,7 @@
 require('mocha');
 const assert = require('assert');
 const through = require('through2');
-const Task = require('../lib/task')(require('@sellside/emitter'));
+const Task = require('../lib/task');
 
 describe('task', function() {
   it('should throw an error when Task is not instantiated', function() {
